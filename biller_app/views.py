a0280fabe6e1c2dbf599e558230058888b71daf7
Aislan Diego <aislandiego@gmail.com>
2018-10-16 18:45:15 -0300

    Creating 2 viewSets for REST API template accordingly documentation

--- biller_app/views.py
+++ biller_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CallStartRecord, CallEndRecord
+from .serializers import CallStartRecordSerializer, CallEndRecordSerializer
+from datetime import datetime, timedelta, tzinfo
 
-# Create your views here.
+
+class CallStartRecordView(viewsets.ModelViewSet):
+    """Django viewSet for the CallStartRecord details receiver"""
+    queryset = CallStartRecord.objects.all()
+    serializer_class = CallStartRecordSerializer
+
+
+class CallEndRecordView(viewsets.ModelViewSet):
+    """Django viewSet for the CallEndRecord details receiver"""
+    queryset = CallEndRecord.objects.all()
+    serializer_class = CallEndRecordSerializer
+
