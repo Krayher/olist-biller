784d0424c50510d58c52e177b8ab2c89bec81966
Aislan Diego <aislandiego@gmail.com>
2018-10-29 01:15:37 -0300

    fixing heroku whitenoise middleware for collectstatic

--- olist_biller/settings.py
+++ olist_biller/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'biller_app.apps.BillerAppConfig',
     'rest_framework',
     'bootstrapform',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 MIDDLEWARE = [
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'olist_biller.urls'
